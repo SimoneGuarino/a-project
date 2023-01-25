41c824e04d9378ec24ae512cfc23ea532ead1463
Danger <simoneguarino_@outlook.it>
2023-01-25 14:35:42 +0100

    first commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+let myVar: number = 3; 
+myVar += 10;
+
+console.log("MyVar = ", myVar);
+
+interface MyInterface {
+    myNumber: number;
+    myString: string;
+}
+
+let myInterface: MyInterface = {
+    myNumber: 10,
+    myString: "Hello World",
+};
+
+console.log("myNumber = ", myInterface.myNumber);
